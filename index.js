b1e6f92fc398ac0dd9a64b6a91230315318294bd
Halil güler <h.guler3717@gmail.com>
2019-07-20 22:02:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 function getFirstSelector(selector){
-  document.getElementById("div").querySelectorAll("div.ranked-list");
+  document.getElementById("div").querySelector("div.ranked-list");
 }
 
 
